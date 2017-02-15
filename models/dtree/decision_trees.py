2afc012e440d418a05a2740ebeb46f8beaa7d019
Georgi Dikov <gvdikov@gmail.com>
2017-02-15 13:16:56 +0100

    refactor the decision trees; TODO make it work with MNIST

--- models/dtree/decision_trees.py
+++ models/dtree/decision_trees.py
@@ -1,11 +1,30 @@
 import numpy as np
 import os
 
-# TODO: adapt the code for the MNIST dataset, inheriting from abstract model
+from models.model import AbstractModel
 
-class DecisionTree():
-    def __init__(self, num_classes, id="root"):
-        self.id = id
+n_classes = 3
+
+class DecisionTree(AbstractModel):
+    def __init__(self):
+        super(DecisionTree, self).__init__('DecisionTree')
+        self.dtree = _RecursiveTree(num_classes=n_classes)
+
+    def fit(self, train_data, **kwargs):
+        depth = kwargs.get('depth', 1)
+        # fit on the whole dataset
+        self.dtree.fit(train_data, depth=depth)
+
+    def predict(self, new_data, **kwargs):
+        predictions = np.zeros(new_data.shape[0])
+        for i, x in enumerate(new_data):
+            predictions[i] = self.dtree.predict(x)
+        return predictions
+
+
+class _RecursiveTree():
+    def __init__(self, num_classes, id_="root"):
+        self.id = id_
         self.is_terminal = True
 
         self.class_dist = np.zeros(num_classes)
@@ -18,14 +37,12 @@ class DecisionTree():
         self.lq = None  # Left DT: less than or equal to the threshold
         self.gt = None  # Right DT: greater than the threshold
 
-        """ Compute the new class and probability distribution as well as the impurity (Gini index) """
 
     def _update_node(self, ys):
         self.class_dist += np.bincount(ys, minlength=self.class_dist.shape[0])
         self.p_dist = self.class_dist / np.sum(self.class_dist)
         self.gini_index = 1 - np.sum(self.p_dist ** 2)
 
-        """ Compute the infomation gain for all possible splittings within a feature and all possible features """
 
     def _compute_igain(self, xs, ys):
         num_examples, num_features = xs.shape
@@ -35,6 +52,7 @@ class DecisionTree():
         if self.is_terminal and self.gini_index > 0.:
             for f in xrange(num_features):
                 for e in xrange(num_examples):
+                    # TODO: refactor the epsilon and compute boundary as middle point of two data points
                     all_lq = (xs[:, f] <= xs[e, f] - 1e-8)
                     num_all_lq = all_lq.sum()
                     p_lq = num_all_lq / float(num_examples)
@@ -59,9 +77,8 @@ class DecisionTree():
 
         return igain_max, e_max, f_id_max, datax_lq, datay_lq, datax_gt, datay_gt
 
-        """ Internal trainig method according to the depth and information gain criteria """
 
-    def _train_on(self, dataset, depth):
+    def fit(self, dataset, depth):
         xs, ys = dataset
         # update root parameters
         self._update_node(ys)
@@ -73,38 +90,25 @@ class DecisionTree():
                 self.is_terminal = False
                 self.decision_threshold = xs[e_max, f_id_max] - 1e-8
                 self.decision_feature_id = f_id_max
-                self.lq = DecisionTree(num_classes=self.class_dist.shape[0], id="lq_{0}".format(depth))
-                self.gt = DecisionTree(num_classes=self.class_dist.shape[0], id="gt_{0}".format(depth))
+                self.lq = _RecursiveTree(num_classes=self.class_dist.shape[0], id_="lq_{0}".format(depth))
+                self.gt = _RecursiveTree(num_classes=self.class_dist.shape[0], id_="gt_{0}".format(depth))
 
-                self.lq.train((datax_lq, datay_lq), depth=depth - 1)
-                self.gt.train((datax_gt, datay_gt), depth=depth - 1)
+                self.lq.fit((datax_lq, datay_lq), depth=depth - 1)
+                self.gt.fit((datax_gt, datay_gt), depth=depth - 1)
 
-        """ Public train method which also estimates the accuracy """
 
-    def train(self, dataset, depth=2):
-        xs, ys = dataset
-        # train on the whole dataset
-        self._train_on(dataset, depth=depth)
-
-        # check the accuracy and error
-        acc = 0
-        for ind, x in enumerate(xs):
-            y, _ = self.test(x)
-            if y == ys[ind]:
-                acc += 1
-        return float(acc) * 100. / xs.shape[0]
-
-        """ Test the decision tree with some unseen example """
-
-    def test(self, x):
+    def predict(self, x, return_probs=False):
         if self.is_terminal:
-            return np.argmax(self.class_dist), self.p_dist[np.argmax(self.class_dist)]
+            if return_probs:
+                return np.argmax(self.class_dist), self.p_dist[np.argmax(self.class_dist)]
+            else:
+                return np.argmax(self.class_dist)
         else:
             if x[self.decision_feature_id] <= self.decision_threshold:
-                return self.lq.test(x)
-            return self.gt.test(x)
+                return self.lq.predict(x)
+            return self.gt.predict(x)
 
-        """ Print attributes of a node """
+
 
     def print_node(self):
         print("-------------------------------------")
@@ -120,7 +124,6 @@ class DecisionTree():
             print("- Decision threshold: {0}".format(self.decision_threshold))
         print("-------------------------------------")
 
-        """ Print the whole tree from the top (root) to bottom (terminal nodes/leafs)"""
 
     def print_tree(self):
         if self.is_terminal:
@@ -129,8 +132,6 @@ class DecisionTree():
             self.print_node()
             self.lq.print_tree()
             self.gt.print_tree()
-
-    """ Load the dataset from a file as numpy arrays """
 
 
 def load_dataset(filename):
@@ -144,46 +145,21 @@ def load_dataset(filename):
     # strip the labels from the features
     train_data = train_data[:, :-1]
 
-    return train_data, labels
-
-    """ Inspect the dataset in 3d plots """
-
-
-def plot_dataset(xs, ys):
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    ax.scatter(xs[:, 0], xs[:, 1], xs[:, 2], s=25, marker='s', lw=0, c=ys)
-    ax.scatter([4.1], [-0.1], [2.2], s=25, marker='*')
-    ax.scatter([6.1], [0.4], [1.3], s=25, marker='*')
-
-    ax.set_xlabel('0')
-    ax.set_ylabel('1')
-    ax.set_zlabel('2')
-
-    plt.show()
+    return np.array(train_data), np.array(labels)
 
 
 
 if __name__ == '__main__':
-    train_data, labels = load_dataset("sample_dataset.csv")
+    train_data = load_dataset("sample_dataset.csv")
 
-    dt = DecisionTree(num_classes=np.max(labels) + 1)
+    model = DecisionTree()
+    depth = 2
 
-    accuracy = dt.train(dataset=(train_data, labels), depth=2)
-    print("Final training accuracy: {0}%".format(accuracy))
-    dt.print_tree()
-    x_a = np.array([4.1, -0.1, 2.2])
-    x_b = np.array([6.1, 0.4, 1.3])
+    model.fit(train_data, depth=depth)
+    predictions = model.predict(train_data[0])
 
-    y_a, p_a = dt.test(x_a)
-    y_b, p_b = dt.test(x_b)
+    test_acc = np.sum(predictions == train_data[1]) / float(predictions.shape[0]) * 100.
+    print("Test accuracy for depth={1}: {0}"
+          .format(test_acc, depth))
 
-    print("x_a was classified as {0} with p(c=y_a | x_a, T) = {1}".format(y_a, p_a))
-    print("x_b was classified as {0} with p(c=y_b | x_b, T) = {1}".format(y_b, p_b))
-
-    # plot_dataset(train_data, labels)
 
